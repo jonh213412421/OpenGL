6b6884a24344bd6df152eae0386aa72f6eaf32cc
João Alves <138532454+jonh213412421@users.noreply.github.com>
2025-01-20 21:20:10 -0300

    Create basic_setup.cpp

---
+++ OpenGL/basic_setup.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+//função do buffer_callback
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+//verifica se tecla está sendo pressionada
+void verificar_input(GLFWwindow *window) 
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+	//função inicializadora
+	glfwInit();
+	//configuração de opções
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//limita os recursos ao opengl_core_profile
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//cria "objeto" janela
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	//mostra mensagem de erro se falha ao criar janela
+	if (window == NULL) {
+		std::cout << "Falha ao criar a janela" << std::endl;
+		return -1;
+	}
+	//faz a janela o "contexto principal na thread
+	glfwMakeContextCurrent(window);
+
+	//verifica se o GLAD foi inicializado
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		std::cout << "Falha ao inicializar o GLAD" << std::endl;
+		return -1;
+	}
+
+	//passa o tamanho da janela para o gl
+	glViewport(0, 0, 800, 600);
+
+	//função para reajustar janela
+	void framebuffer_size_callback(GLFWwindow * window, int width, int height);
+
+	//chama resize
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//render loop. É aqui que roda tudo!
+	while (!glfwWindowShouldClose(window)) {
+		verificar_input(window);
+		//limpa buffer de cor
+		glClearColor(0.2f, 0.5f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//finaliza programa
+	glfwTerminate();
+
+	return 0;
+}
